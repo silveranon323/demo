1b221e1acf289e413be97f76f1f5816c45ae4bb3
Hem Kishore Pradhan <101815577+silveranon323@users.noreply.github.com>
2025-08-11 22:54:03 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Testing Spring Boot Deployment";
+    }
+}
